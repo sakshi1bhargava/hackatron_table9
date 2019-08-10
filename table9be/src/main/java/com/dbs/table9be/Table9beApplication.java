19e05d5f76cafb076b619af974e2ce9d05866b03
regan <yugi@gmail>
2019-08-10 08:47:35 +0530

    Commiting new Project

---
+++ table9be/src/main/java/com/dbs/table9be/Table9beApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.table9be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Table9beApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Table9beApplication.class, args);
+	}
+
+}
---
+++ table9be/src/test/java/com/dbs/table9be/Table9beApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbs.table9be;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Table9beApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
